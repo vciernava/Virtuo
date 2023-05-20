b080b0c2b6e15e21a0020a72ded6573ff6f9454f
Viktor Čierňava <kontakt@viktorciernava.eu>
2023-05-20 03:54:59 +0200

    Initial commit

---
+++ cmd/webservice.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func Execute() {
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	log.Printf("Open http://localhost:%s in the browser", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
